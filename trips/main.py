f9054ccb8c11bcd2cdb1547ae5614c48f338c0f6
Leo <leogazziro@gmail.com>
2022-03-01 00:44:45 -0300

    to use poetry with docker

---
+++ trips/main.py
@@ -0,0 +1,25 @@
+"""Entry point for trips ingestion."""
+from src.config import settings, logger
+from src.csv_to_db import csv_to_postgres
+from src.execute_db_script import execute
+from src.database.postgres import PostgreConnections
+
+
+def trips_ingestion():
+    """
+    Start trips CSV ingestion
+    :return: None
+    """
+    try:
+        pg = PostgreConnections()
+
+        csv_to_postgres(pg.get_pg_engine({'options': '-csearch_path={}'.format(settings.PG_SCHEMA)}), 'input_files/trips.csv', 'trips_log')
+
+        pg_conn, pg_cursor = pg.get_pg_connection_and_cursor()
+        execute(pg_conn, pg_cursor, 'create_trips_log.sql')
+    except Exception as err:
+        logger.error(err)
+
+
+if __name__ == '__main__':
+    trips_ingestion()
